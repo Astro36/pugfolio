414a52ef445a06dfb907edea41d7c7b918c8b0a6
Astro36 <astr36@naver.com>
2017-07-27 15:53:28 +0900

    Initial Commit

---
+++ bin/index.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const path = require('path');
+const pugfolio = require('../')
+
+const args = process.argv.slice(2);
+
+if (args.length <= 2) {
+  pugfolio.render(args[0], args[1]);
+}
+
