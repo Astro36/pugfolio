e39e8aee60770a82b826a10b157709b0a7c3b3a0
Astro36 <astr36@naver.com>
2017-10-24 01:17:42 +0900

    Add webpack

---
+++ themes/skeleton/main.js
@@ -0,0 +1,17 @@
+import './assets/css/font-awesome.min.css';
+import './assets/css/katex.min.css';
+import './assets/css/prism.css';
+import './assets/css/skeleton.css';
+import './assets/css/custom.css';
+import './assets/css/spoqa-han-sans-kr.css';
+import './assets/js/katex.min.js';
+import './assets/js/katex-auto.min.js';
+import './assets/js/prism.min.js';
+
+renderMathInElement(document.body, {
+  delimiters: [
+    { left: "$$", right: "$$", display: true },
+    { left: "\\[", right: "\\]", display: true },
+    { left: "\\(", right: "\\)", display: false }
+  ]
+});
