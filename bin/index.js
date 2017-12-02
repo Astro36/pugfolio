6f24d4a8371dd988d8fb70067f17a375bb6db015
MCPE_PC <maxpjh0528@naver.com>
2017-12-02 15:27:40 +0900

    Fix a bug

--- bin/index.js
+++ bin/index.js
@@ -3,7 +3,7 @@
 const pugfolio = require('../')
 , args = process.argv.slice(2);
 
-if (args.length === 0) pugfolio.render();
-else if (args.length === 1) pugfolio.render(args[0]);
-else if (args.length === 2) pugfolio.render(args[0], args[1]);
+if (args.length === 0) pugfolio();
+else if (args.length === 1) pugfolio(args[0]);
+else if (args.length === 2) pugfolio(args[0], args[1]);
 else throw new RangeError('The maximum number of arguments is two.');
