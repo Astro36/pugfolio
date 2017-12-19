b18f5d359d386c1dee334e53050ccbc03ea7b515
Astro36 <astr36@naver.com>
2017-12-19 14:58:51 +0900

    Update packages and lint the codes

--- bin/index.js
+++ bin/index.js
@@ -1,9 +1,15 @@
 #!/usr/bin/env node
 
-const pugfolio = require('../')
-, args = process.argv.slice(2);
+const pugfolio = require('../');
 
-if (args.length === 0) pugfolio.render();
-else if (args.length === 1) pugfolio.render(args[0]);
-else if (args.length === 2) pugfolio.render(args[0], args[1]);
-else throw new RangeError('The maximum number of arguments is two.');
+const args = process.argv.slice(2);
+
+if (args.length === 0) {
+  pugfolio.render();
+} else if (args.length === 1) {
+  pugfolio.render(args[0]);
+} else if (args.length === 2) {
+  pugfolio.render(args[0], args[1]);
+} else {
+  throw new RangeError('The maximum number of arguments is two.');
+}
